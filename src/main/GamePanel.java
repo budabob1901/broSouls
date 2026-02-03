20f55497d3815422670f70b309f6ff7f09af1176
budabob1901 <kjemsfrederik@gmail.com>
2026-02-03 15:59:05 +0100

    man kan nu styre firkanten med WASD :))

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -14,12 +14,24 @@ public class GamePanel extends JPanel implements Runnable {
     final int screenWidth = tileSize * maxScreenCol; //768 pixels
     final int screenHeight = tileSize * maxScreenRow;  // 576 pixels
 
+    //FPS
+    int FPS = 60;
+
+    KeyHandler keyH = new KeyHandler();
     Thread gameThread;
+
+    // set player default position
+
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
 
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
 
     }
 
@@ -32,19 +44,51 @@ public class GamePanel extends JPanel implements Runnable {
 
     @Override
     public void run() {
-     while(gameThread != null){
+        double drawInterval = 1000000000/FPS; // TEGNER BILLEDET HERT 0,01666 SEKUND
+        double nextDrawTime = System.nanoTime() + drawInterval;
 
-       //  System.out.println("The game loop is running");
+     while(gameThread != null){
          // 1 UPDATE: update information such as charchter position
          update();
+
          // 2 DRAW: draw the screen with the updated information
          repaint();
+
+
+
+         try {
+             double remainingTime = nextDrawTime - System.nanoTime();
+             remainingTime = remainingTime/1000000;
+
+             if(remainingTime < 0) {
+                 remainingTime = 0;
+             }
+             Thread.sleep((long) remainingTime);
+
+             nextDrawTime += drawInterval;
+
+         } catch (InterruptedException e) {
+             e.printStackTrace();
+         }
      }
 
     }
 
+     // i jave starter kordnator 0x, 0y i vesnte øverste side af hjørnet.
     public void update() {
+       if(keyH.upPressed == true) {
+           playerY -= playerSpeed;
+       }
+       else if (keyH.downPressed == true){
+           playerY += playerSpeed;
 
+       } else if (keyH.leftPressed == true) {
+           playerX -= playerSpeed;
+           
+       } else if (keyH.rightPressed == true) {
+            playerX += playerSpeed;
+           
+       }
     }
     //Graphic is a class methods to draw object on our screen
     public void paintComponent(Graphics g) {
@@ -55,7 +99,7 @@ public class GamePanel extends JPanel implements Runnable {
 
         g2.setColor(Color.white);
         //DRAWS A RECTANGLE
-        g2.fillRect(100, 100, tileSize, tileSize);
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
 
         //program works without this but save memory
         g2.dispose();
--- src/main/KeyHandler.java
+++ src/main/KeyHandler.java
@@ -1,4 +1,51 @@
 package main;
 
-public class KeyHandler {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyHandler implements KeyListener {
+    //instans datatype boolean
+    public boolean upPressed, downPressed, leftPressed, rightPressed;
+
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+      int code = e.getKeyCode();
+
+      if (code ==KeyEvent.VK_W){
+          upPressed = true;
+      }
+      if (code ==KeyEvent.VK_S){
+          downPressed = true;
+        }
+      if (code ==KeyEvent.VK_A){
+          leftPressed = true;
+        }
+      if (code ==KeyEvent.VK_D){
+         rightPressed = true;
+        }
+    }
+    // til at registere at knappen er blevet sluppet
+    @Override
+    public void keyReleased(KeyEvent e) {
+         int code = e.getKeyCode();
+
+        if (code ==KeyEvent.VK_W){
+            upPressed = false;
+        }
+        if (code ==KeyEvent.VK_S){
+            downPressed = false;
+        }
+        if (code ==KeyEvent.VK_A){
+            leftPressed = false;
+        }
+        if (code ==KeyEvent.VK_D){
+            rightPressed = false;
+        }
+    }
 }
